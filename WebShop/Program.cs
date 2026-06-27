46ae737272fed479c230e55b2ba0a30e0307aac8
Anders Johnsson <ronnebyanders@hotmail.com>
2026-06-27 17:14:33 +0200

    Added Experiments for redis-sessions in Program.cs and an ReadMe.txt-file

--- WebShop/Program.cs
+++ WebShop/Program.cs
@@ -1,4 +1,6 @@
+using Azure;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
@@ -26,30 +28,40 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddRoles<IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 
 
-//var redisString = builder.Configuration["ConnectionStrings:RedisConn"]??"";   // secrets.json
+var redisString = builder.Configuration["ConnectionStrings:RedisConn"]??"";   // secrets.json  ,   WS/WC
 
-//if (redisString != "")
-//{
-//    var redisOptions = new ConfigurationOptions     // Experiment med Redis-server för sessioner. C har en server som kör på
-//    {                                               // Endpointen nedan. Gäller bara i hans nätverk.
-//        EndPoints = { redisString },
-//        DefaultDatabase = 2,
-//        Ssl = true,                     // set true only if your Redis uses TLS
-//        AbortOnConnectFail = false
-//    };
-//}
+var redisOptions = new ConfigurationOptions();    // Default, empty options, used if no Redis connection string is provided. Will cause an error if Redis is attempted to be used without a valid connection string. 
+if (redisString != "")         // WS/WC
+{
+    redisOptions = new ConfigurationOptions     // Experiment med Redis-server för sessioner. C har en server som kör på
+    {                                               // Endpointen nedan. Gäller bara i hans nätverk.
+        EndPoints = { redisString },
+        DefaultDatabase = 2,
+        Ssl = true,                     // set true only if your Redis uses TLS
+        AbortOnConnectFail = false,
+        ConnectTimeout = 5000
+        
+    };
+}
+else
+{
+       Console.WriteLine("No Redis connection string found. Redis caching and sessions will not work.");
+       throw new Exception("No Redis connection string found. Redis caching and sessions will not work.");
+}
 
-//builder.Services.AddStackExchangeRedisCache(options =>      // Endast för test av cachning med Redis på WeatherService
-//{
-//    options.ConfigurationOptions = redisOptions;
-//    options.InstanceName = "WeatherService:";
-//});
+    builder.Services.AddStackExchangeRedisCache(options =>      // Endast för test av cachning med Redis på WeatherService    WS/WC
+    {
+        options.ConfigurationOptions = redisOptions;            
+        options.InstanceName = "WeatherService:";
+      
+    });
 
 
 builder.Services.AddSession(opts =>                  // Session for in-memory but also for redis-sessions, These settings will create an extra cookie 
 {                                                         // on some pages because of .Net core data protection
-    opts.IdleTimeout = TimeSpan.FromMinutes(20);       // change time in real app. No session-store set means in-memory-sessions.
-    //opts.Cookie.Name = "sid";
+    opts.IdleTimeout = TimeSpan.FromMinutes(20);       // change time in real app. 
+    //opts.Cookie.Name = "sid";                         No session-store set means in-memory-sessions.
+
     opts.Cookie.HttpOnly = true;
     opts.Cookie.IsEssential = true;
     opts.Cookie.SecurePolicy = CookieSecurePolicy.Always;   // Only https session requests
@@ -57,13 +69,14 @@ builder.Services.AddSession(opts =>                  // Session for in-memory bu
 });
 
 
-/*builder.Services.AddSingleton<WeatherService>();*/    // till redis
+builder.Services.AddSingleton<WeatherService>();    // till redis: Alla ska få samma WeatherService     WS/WC
 
 
-//var redis = ConnectionMultiplexer.Connect(redisOptions);
-//var db = redis.GetDatabase(2); // DB 2 over TLS
+var redis = ConnectionMultiplexer.Connect(redisOptions);   // SESS
+var db = redis.GetDatabase(2); // DB 2 over TLS          SESS
 
-//db.StringSet("session:123", "active");   just an example
+
+//db.StringSet("session:123", "active");   //just an example
 
 
 
@@ -103,23 +116,48 @@ app.UseSession();                               // Session
 
 // app.useOutputCaching();                          // Suggestion: Output caching, requires .AddOutputCaching() in services
 
-
-//app.MapGet("/set", async (HttpContext ctx) =>   // Experimentell minimal endpoint för att testa Redis-sessioner
+//app.Use(async (context, next) =>
 //{
-//    var sessionId = Guid.NewGuid().ToString("N");
+//    context.Response.Cookies.Append(
+//        "text",
+//        "Hoola B",
+//        new CookieOptions
+//        {
+//            HttpOnly = true,
+//            Secure = true,
+//            SameSite = SameSiteMode.Strict,
+//            Expires = DateTimeOffset.UtcNow.AddMinutes(2)
+//        }
+//    );
 
-//    var guid = "6767dferlpbj8989dhfhh";
-//    ctx.Response.Cookies.Append("sid", sessionId, new CookieOptions
-//    {
-//        HttpOnly = true,
-//        Secure = true,
-//        SameSite = SameSiteMode.Strict,
-//        Expires = DateTimeOffset.UtcNow.AddMinutes(2)
-//    });
-//    var jsonData = $"{{ \"Guid\" : \"{guid}\", \"Name\": \"Anders\", \"Age\": 42 }}";
-//    await db.StringSetAsync($"session:{sessionId}", jsonData, TimeSpan.FromMinutes(2));    //new { Guid = guid, Name = "Anders", Age = 42 }    "{\"userId\":45,\"guid\":guid}"
-//    return Results.Ok("Stored");
+//    await next();
 //});
+
+app.MapGet("/set", async (HttpContext ctx) =>   // Experimentell minimal endpoint för att testa Redis-sessioner     SESS
+{
+    var sessionId = Guid.NewGuid().ToString("N");
+
+    var guid = "6767dferlpbj8989dhfhh";
+    ctx.Response.Cookies.Append("sid", sessionId, new CookieOptions
+    {
+        HttpOnly = true,
+        Secure = true,
+        SameSite = SameSiteMode.Strict,
+        Expires = DateTimeOffset.UtcNow.AddMinutes(2)
+    });
+    ctx.Response.Cookies.Append("test", "45454", new CookieOptions
+    {
+        HttpOnly = true,
+        Secure = true,
+        SameSite = SameSiteMode.Strict,
+        Expires = DateTimeOffset.UtcNow.AddMinutes(2)
+    });
+    var jsonData = $"{{ \"Guid\" : \"{guid}\", \"Name\": \"Anders\", \"Age\": 42 }}";
+    await db.StringSetAsync($"session:{sessionId}", jsonData, TimeSpan.FromMinutes(2));    //new { Guid = guid, Name = "Anders", Age = 42 }    "{\"userId\":45,\"guid\":guid}"
+   
+    return Results.Ok(ctx.Request.Cookies["test"]);      
+});
+
 
 
 //app.MapGet("/setwrong", (HttpContext ctx) =>
@@ -133,6 +171,15 @@ app.UseSession();                               // Session
 //    return "Stored in Redis session";
 //});
 
+//app.Use(async (context, next) =>
+//{
+//    if (context.Request.Path == "/set")
+//    {
+//        context.Response.Cookies.Append("test", "hoola");
+//    }
+
+//    await next();
+//});
 
 
 // This warms up the DB connection on application start making the first sql query faster.
@@ -140,7 +187,7 @@ app.Lifetime.ApplicationStarted.Register(async () =>
 {
     using var scope = app.Services.CreateScope();
     var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    await db.Database.ExecuteSqlRawAsync("SELECT 1");   // Never use ExecuteSqlRawAsync otherwise.
+    await db.Database.ExecuteSqlRawAsync("SELECT 1");   // Never use ExecuteSqlRawAsync otherwise.   
 });
 
 
---
+++ WebShop/ReadMe.txt
@@ -0,0 +1,6 @@
+﻿
+Detta program ansluter till C's Redis-databas genom att ansluta från mitt YNWA till C's YNWA.(YNWA->YNWA)
+Jag måste alltså använda mitt YNWA-nätverk hemma (inte Andy) för att kunna köra programmet.
+Annars får man connection-timeout p.g.a rad 42 i Program.cs.
+
+Kommentera ut redis-anslutningarna för att köra sessions med den gamla InmemoryCache istället.
